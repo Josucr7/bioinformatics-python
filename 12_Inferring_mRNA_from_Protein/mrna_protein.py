d59d3708136a7eee0e9ffadf65505053084dfbbf
josue <jcruz9670u@gmail.com>
2026-08-07 15:33:54 -0500

    Add the modulo to Reductions of List.

--- 12_Inferring_mRNA_from_Protein/mrna_protein.py
+++ 12_Inferring_mRNA_from_Protein/mrna_protein.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
-from typing import NamedTuple,TextIO
+from typing import NamedTuple, List
 import os
 import math
-
+from functools import reduce
 class Args(NamedTuple):
     """ Command lina-arguments. """
 
     protein: str
     modulo: int
+    all:bool
 
 def get_args() -> Args:
     """ Get command line arguments. """
@@ -20,12 +21,14 @@ def get_args() -> Args:
 
     parse.add_argument('-m','--modulo',help="Modulo value",metavar="INT",type=int,default=1000000)
 
+    parse.add_argument('-a','--all',help="Show all combinations.",action='store_true')
+
     args = parse.parse_args()
 
     if os.path.isfile(args.protein):
         args.protein = open(args.protein).read().rstrip()
 
-    return Args(protein=args.protein,modulo=args.modulo)
+    return Args(protein=args.protein,modulo=args.modulo,all=args.all)
 
 aa_to_mrna = {
     '*': ['UAA', 'UAG', 'UGA'],  # Stop Codons
@@ -69,13 +72,36 @@ def product(values:list) -> int:
 
     return math.prod(values)
 
+def mulmod(a: int, b: int, mod: int) -> int:
+    """ Multiplication with modulo """
+
+    def maybemod(x):
+        ret = (x % mod) if mod > 1 and x > mod else x
+        return ret or x
+    res = 0
+    a = maybemod(a)
+    while b > 0:
+        if b % 2 == 1:
+            res = maybemod(res + a)
+        a = maybemod(a * 2)
+        b //= 2
+    return res
+
+def modprod(xs: List[int], modulo: int) -> int:
+    """ Return the product modulo a value """
+
+    return reduce(lambda x, y: mulmod(x, y, modulo), xs, 1)
+
 def main() -> None:
     """ Run code. """
 
     args = get_args ()
     values = posibles_mrna(args.protein)
+    res=modprod(xs=values,modulo=args.modulo)
     result = product(values)
-    print(result)
+    if args.all:
+        print(result)
+    print(res)
 
 if __name__ == "__main__":
     main()
