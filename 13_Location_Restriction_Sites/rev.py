57b5272576938fe8dfc306442f0a308059734386
josue <jcruz9670u@gmail.com>
2026-08-21 22:54:36 -0500

    Read FASTA file and get reverse complement sequence.

---
+++ 13_Location_Restriction_Sites/rev.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import argparse
+from typing import NamedTuple, TextIO
+from Bio import SeqIO, Seq
+
+class Args(NamedTuple):
+    """ Command line-arguments """
+
+    file: TextIO
+
+def get_args() -> Args:
+    """ Get command line-arguments """
+
+    parser = argparse.ArgumentParser(description="Locating Restriction Files.",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',metavar="FILE",help="Input a FASTA file.",type=argparse.FileType("rt"))
+
+    args = parser.parse_args()
+
+    return Args(file=args.file)
+
+def read_fasta_file(file:TextIO) -> str:
+    """ Read a fasta file. """
+
+    rec = SeqIO.parse(file,"fasta")
+    return str(next(rec).seq)
+
+def reverse_complement(seq:str):
+    """ Generate the reverse complement sequences. """
+
+    return Seq.reverse_complement(seq)
+
+def main():
+    """ Run code """
+
+    args = get_args()
+
+    seq = read_fasta_file(args.file)
+    rev = reverse_complement(seq)
+    print(rev)
+
+if __name__ == "__main__":
+    main()
