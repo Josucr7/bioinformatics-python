1765e852936bf34060177fbb09381e9cb7af0139
josue <jcruz9670u@gmail.com>
2026-08-22 19:04:53 -0500

    Identify the restriction sites

--- 13_Location_Restriction_Sites/rev.py
+++ 13_Location_Restriction_Sites/rev.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from typing import NamedTuple, TextIO, Optional
+from typing import NamedTuple, TextIO, Optional, List
 from Bio import SeqIO, Seq
 
 class Args(NamedTuple):
@@ -31,22 +31,28 @@ def reverse_complement(seq:str):
 
     return Seq.reverse_complement(seq)
 
-def k_mers(sequence:str,k:int) -> Optional[str]:
+def k_mers(sequence:str,k:int) -> Optional[List]:
     """ Get k-mers from a DNA sequences. """
 
     n = len(sequence) + 1 - k
     return [] if n < 1 else [sequence[i:i+k] for i in range(n)]
-     
 
+def compare_kmer(k_mers:list) -> Optional[List]:
+    """ Detect if k-mers has equality from sequences.. """
+
+    reverse = [reverse_complement(k_mer) for k_mer in k_mers]
+    base = list(enumerate(zip(k_mers,reverse),start=1))
+    positions = [(position,len(sequences[0])) for position,sequences in base if sequences[0]==sequences[1]] 
+    if not positions:
+        return None
+    return positions
 def main():
     """ Run code """
 
     args = get_args()
-
-    seq = read_fasta_file(args.file)
-    rev = reverse_complement(seq)
-    print(rev)
-    print(k_mers(seq,3))
-
+    sequence = read_fasta_file(args.file)
+    sites=[ site for i in range(4,len(sequence)) if (site:=compare_kmer(k_mers(sequence,i))) is not None]
+    for site in sites:
+        print(site)
 if __name__ == "__main__":
     main()
