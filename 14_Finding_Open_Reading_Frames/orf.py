1009e23f03f0e6186d4b4a049ecc2a0388735aea
josue <jcruz9670u@gmail.com>
2026-08-26 13:35:06 -0500

    Find the ofrs in protein sequences

--- 14_Finding_Open_Reading_Frames/orf.py
+++ 14_Finding_Open_Reading_Frames/orf.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import argparse
-from typing import NamedTuple, TextIO, Optional
+from typing import NamedTuple, TextIO, Optional, Tuple, List
 from Bio import SeqIO, Seq
+import re
+import itertools
 
 class Args(NamedTuple):
     """ Command line-Arguments."""
@@ -28,17 +30,38 @@ def read_fasta_file(file:TextIO) -> Optional[str]:
         return str(sequence.seq)
     return None
 
-def dna_to_aa(dna:str) -> str:
+def dna_to_aa(dna:str) -> Tuple[str]:
     """ From DNA sequence translate to aminoacid sequence.  """
+    rna = Seq.transcribe(dna)
+    rna_reverse = Seq.reverse_complement_rna(rna)
+    aa = Seq.translate(rna)
+    aa_1 = Seq.translate(rna[1:])
+    aa_2 = Seq.translate(rna[2:])
+    aar = Seq.translate(rna_reverse)
+    aar_1 = Seq.translate(rna_reverse[1:])
+    aar_2 = Seq.translate(rna_reverse[2:])
+    return aa,aar,aa_1,aar_1,aa_2,aar_2
 
-    return Seq.translate(dna,to_stop=True)
+def truncate(seq: str, k: int) -> str:
+    """ Truncate a sequence to even division by k """
 
+    return ''
+def find_orfs(aa: str) -> List[str]:
+    """ Find ORFs in AA sequence """
+
+    regex = re.compile('(?=(M[A-Z]*[*$]))')
+    coincidence = regex.findall(aa)
+    return coincidence
 
 def main() -> None:
     """ Run code. """
+
     args = get_args()
     sequence = read_fasta_file(args.file)
-    print(dna_to_aa(sequence))
+    aa = dna_to_aa(sequence)
+    orf = [find_orfs(seq) for seq in aa ] 
+    result = set(itertools.chain.from_iterable(orf))
+    print(*result)
 
 if __name__ == "__main__":
     main()
